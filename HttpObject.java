593d89fec3cda4a10c193fbda9135368650ca86f
Amelia Williams <86878748+Melidee@users.noreply.github.com>
2023-05-15 08:36:28 -0500

    Update HttpObject.java

--- HttpObject.java
+++ HttpObject.java
@@ -2,22 +2,22 @@ import java.util.HashMap;
 
 public class HttpObject {
 
-    public final HashMap<String, String> headers;
+    public final HashMap<String, String> headers; // http headers are common between request and response
 
-    public HttpObject() {
+    public HttpObject() { // generic constructor
         this.headers = new HashMap<>();
     }
 
-    public HttpObject addHeader(String key, String val) {
+    public HttpObject addHeader(String key, String val) { // mutator
         headers.put(key, val);
         return this;
     }
 
-    public String getHeader(String key) {
+    public String getHeader(String key) { // accessor
         return headers.get(key);
     }
 
-    public String format() {
+    public String format() { // format the headers, needs to be extended to create a valid request
         String out = "";
         for (String key : headers.keySet()) {
             out += key + ": " + headers.get(key) + "\r\n";
