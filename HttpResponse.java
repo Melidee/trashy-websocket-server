69a5acc7a069c76deaa57172489510adbd54f572
Amelia Williams <86878748+Melidee@users.noreply.github.com>
2023-05-15 08:46:43 -0500

    Update HttpResponse.java

--- HttpResponse.java
+++ HttpResponse.java
@@ -7,23 +7,23 @@ public class HttpResponse extends HttpObject {
     private final String status;
     private String content;
 
-    public HttpResponse(String status) {
+    public HttpResponse(String status) { // http responses MUST have a status code, this tells the client how their request was interpreted
         super();
         this.status = status;
         this.content = "";
     }
 
-    public HttpResponse setContent(String newContent) {
+    public HttpResponse setContent(String newContent) { // correctly sets the content length
         this.content = newContent;
         headers.put("Content-Length", String.valueOf(content.getBytes().length));
         return this;
     }
 
-    public String getContent() {
+    public String getContent() { // getter
         return content;
     }
 
-    public int getContentLength() {
+    public int getContentLength() { // http requires a content-length header for content, this is not used in this codebase though
         if (content.equals("")) {
             return 0;
         }
@@ -32,16 +32,15 @@ public class HttpResponse extends HttpObject {
 
     @Override
     public String format() {
-        return "HTTP/1.1 " + status + "\r\n" + super.format();
+        return "HTTP/1.1 " + status + "\r\n" + super.format(); // formats the properties into an http response
     }
 
+    // to generate the websocket accept you must concatenate the client sec websocket key with a magic string, then sha1 hash it
     public static String genWebsocketAccept(String websocketKey) {
         if (digest == null) {
             try {
                 digest = MessageDigest.getInstance("SHA-1");
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            }
+            } catch (NoSuchAlgorithmException e) {} // never throws
         }
         String combined = websocketKey.strip() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
         byte[] accept = digest.digest(combined.getBytes());
